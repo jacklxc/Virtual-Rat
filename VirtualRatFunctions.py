c2fc5da82779c6ca37a00c07479c3dcdcfa50d9f
Xiangci Li <xl1066@nyu.edu>
2016-07-18 21:05:57 +0800

    Wrote code for figure 3-d plot

--- VirtualRatFunctions.py
+++ VirtualRatFunctions.py
@@ -19,7 +19,7 @@ def getData(num_rats=15):
     allRatsData = {}
 
     if num_rats > len(all_rats):
-    	num_rats = 15
+        num_rats = 15
 
     for rat in all_rats[:num_rats]:
         sqlstr=('select pro_rule, target_on_right, trial_n=1, '
@@ -55,109 +55,176 @@ def uploadRNN(solver, ratname, comments, test_size, lr, hidden_dim, acc):
 
     print dbc.saveToDB('vrat.rnn',D)
 
-def processData(allRatsData):
-	"""
-	Divide each rat's data into train, validation and test data for RNN
+def preProcess(allRatsData):
+    """
+    Divide each rat's data into train, validation and test data for RNN
 
-	Inputs:
-	- allRatsData: a dictionary with rat names as keys and numpy boolean array of 
+    Inputs:
+    - allRatsData: a dictionary with rat names as keys and numpy boolean array of 
     shape N * T * D as elements. In dimention 2, the bits represent pro_rule,
     target_on_right, trial_n=1, left, right, cpv (central poke violation) respectively.
 
     Returns:
     - data: a dictionary with rat names as keys and dictionaries as elements.
-    	Each sub-level dictionary contains the following:
-    	- 'trainX': Stimulus to the rat for training.
-    	- 'valX': Stimulus to the rat for validation.
-    	- 'testX': Stimulus to the rat for test.
-    	- 'trainY': Reactions of the rat for training.
-    	- 'valY': Reactions of the rat for validation.
-    	- 'testY': Reactions of the rat for test.
-    	- 'trainTrueY': Rational reactions (logically correct reactions) for training.
-    	- 'valTrueY': Rational reactions (logically correct reactions) for validation.
-    	- 'testTrueY': Rational reactions (logically correct reactions) for test.
-	"""
-	data = {}
+        Each sub-level dictionary contains the following:
+        - 'trainX': Stimulus to the rat for training.
+        - 'valX': Stimulus to the rat for validation.
+        - 'testX': Stimulus to the rat for test.
+        - 'trainY': Reactions of the rat for training.
+        - 'valY': Reactions of the rat for validation.
+        - 'testY': Reactions of the rat for test.
+        - 'trainTrueY': Rational reactions (logically correct reactions) for training.
+        - 'valTrueY': Rational reactions (logically correct reactions) for validation.
+        - 'testTrueY': Rational reactions (logically correct reactions) for test.
+    """
+    data = {}
 
-	train_percentile = 0.8
-	val_percentile = 0.9
+    train_percentile = 0.8
+    val_percentile = 0.9
 
-	for ratname in allRatsData.keys():
-		data[ratname] = {}
-		rat_data = data[ratname]
-		rat = allRatsData[ratname]
-		x = np.zeros((1, rat.shape[0], 3))
-		y = np.zeros((1, rat.shape[0]))
-		trueY = np.zeros((1, rat.shape[0]))
+    for ratname in allRatsData.keys():
+        data[ratname] = {}
+        rat_data = data[ratname]
+        rat = allRatsData[ratname]
+        x = np.zeros((1, rat.shape[0], 3))
+        y = np.zeros((1, rat.shape[0]))
+        trueY = np.zeros((1, rat.shape[0]))
 
-		x[0,:,:] = rat[:,:3]
+        x[0,:,:] = rat[:,:3]
 
-		train_num = int(rat.shape[0] * train_percentile)
-		val_num = int(rat.shape[0] * val_percentile)
+        train_num = int(rat.shape[0] * train_percentile)
+        val_num = int(rat.shape[0] * val_percentile)
 
-		# Stimulus to rats
-		rat_data['trainX'] = x[:,:train_num,:]
-		rat_data['valX'] = x[:,train_num:val_num,:]
-		rat_data['testX'] = x[:,val_num:,:]
+        # Stimulus to rats
+        rat_data['trainX'] = x[:,:train_num,:]
+        rat_data['valX'] = x[:,train_num:val_num,:]
+        rat_data['testX'] = x[:,val_num:,:]
 
-		# Reaction of rats
-		y[0,rat[:,3]>0] = 0
-		y[0,rat[:,4]>0] = 1
-		y[0,rat[:,5]>0] = 2
+        # Reaction of rats
+        y[0,rat[:,3]>0] = 0
+        y[0,rat[:,4]>0] = 1
+        y[0,rat[:,5]>0] = 2
 
-		rat_data['trainY'] = y[:,:train_num]
-		rat_data['valY'] = y[:,train_num:val_num]
-		rat_data['testY'] = y[:,val_num:]
+        rat_data['trainY'] = y[:,:train_num]
+        rat_data['valY'] = y[:,train_num:val_num]
+        rat_data['testY'] = y[:,val_num:]
 
-		# Rational reaction (logically correct)
-		trueY[0,:] = np.logical_not(np.bitwise_xor(rat[:,0],rat[:,1]))
-		rat_data['trainTrueY'] = trueY[:,:train_num]
-		rat_data['valTrueY'] = trueY[:,train_num:val_num]
-		rat_data['testTrueY'] = trueY[:,val_num:]
+        # Rational reaction (logically correct)
+        trueY[0,:] = np.logical_not(np.bitwise_xor(rat[:,0],rat[:,1]))
+        rat_data['trainTrueY'] = trueY[:,:train_num]
+        rat_data['valTrueY'] = trueY[:,train_num:val_num]
+        rat_data['testTrueY'] = trueY[:,val_num:]
 
-	return data
+    return data
 
-def postRNNdataProcessing(probabilities, allRatsData):
-	postRNNdata = {}
-	for ratname in probabilities:
-		postRNNdata[ratname] = {}
-		ratProbs = postRNNdata[ratname]
+def postProcess(probabilities, allRatsData, trial_window = 3):
+    """
+    Process data generated from RNN. 
 
-		probs = probabilities[ratname]
-		rat_data = allRatsData[ratname]
+    Inputs:
+    -probabilities: Data output from RNN. Array of shape (N, T, DD) giving the probability of each choice,
+          where each element is a float. 
+    -allRatsData: a dictionary with rat names as keys and numpy boolean array of 
+        shape N * T * D as elements. In dimention 2, the bits represent pro_rule,
+        target_on_right, trial_n=1, left, right, cpv (central poke violation) respectively.
 
-		normalized_probs = np.zeros((probs.shape[0],probs.shape[1],probs.shape[2]-1))
-		normalized_probs[:,:,0] = probs[:,:,0]/(probs[:,:,0] + probs[:,:,1])
-		normalized_probs[:,:,1] = probs[:,:,1]/(probs[:,:,0] + probs[:,:,1])
-		postRNNdata['normalized_probs'] = normalized_probs
+    Outputs:
+    -postRNNdata: a dictionary with rat names as keys and dictionaries as elements.
+        Each sub-level dictionary contains the following:
+        -'normalized_probs': numpy float array of shape (N * T * 2). Probablities that discarded cpv and normalized only for left and right.
+        -'hit_rate': numpy float array of shape (T,). Correct probability for each trial (discard cpv).
+        - 'switches': python int list that contains the trial number that switches pro_rule.
+        - 'p2a_switch': python int list that contains the trial number that switches pro_rule form "pro" to "anti".
+        - 'a2p_switch': python int list that contains the trial number that switches pro_rule form "anti" to "pro". 
+        - 'pro_rules': a numpy bool array of shape (T,). Contains the pro_rule for each trial.
+        - 'pro_prob': a numpy float array that only contains the probability of correct orientation under "pro" rule.
+            At those indices (trials) that is under "anti" rules, the element is None.
+        - 'anti_prob': a numpy float array that only contains the probability of correct orientation under "anti" rule.
+            At those indices (trials) that is under "pro" rules, the element is None.
+        - 'a2p_prob': a numpy float array of shape (2 * trial_window + 1,). Mean probabilities of correct orientation before 
+            and after each pro_rule switch form anti to pro.
+        - 'p2a_prob': a numpy float array of shape (2 * trial_window + 1,). Mean probabilities of correct orientation before 
+            and after each pro_rule switch form pro to anti.
+            (index i --> trial from switch = -trial_window + i)
+    """
+    postRNNdata = {}
+    for ratname in probabilities:
+        postRNNdata[ratname] = {}
+        ratProbs = postRNNdata[ratname]
 
-		hit_rate = np.zeros(probs.shape[1])
-		right = rat_data['valTrueY'][0,:] == 1
-		left = rat_data['valTrueY'][0,:] == 0
-		hit_rate[left] = normalized_probs[0,left,0]
-		hit_rate[right] = normalized_probs[0,right,1]
-		postRNNdata['hit_rate'] = hit_rate
+        probs = probabilities[ratname]
+        rat_data = allRatsData[ratname]
 
-		switches = []
-		pro_rule = 0
-		for i in xrange(rat_data['valX'].shape[1]):
-		    if rat_data['valX'][0,i,0] != pro_rule:
-		        switches.append(i)
-		        pro_rule = rat_data['valX'][0,i,0]
-		postRNNdata['switches'] = switches
+        normalized_probs = np.zeros((probs.shape[0],probs.shape[1],probs.shape[2]-1))
+        normalized_probs[:,:,0] = probs[:,:,0]/(probs[:,:,0] + probs[:,:,1])
+        normalized_probs[:,:,1] = probs[:,:,1]/(probs[:,:,0] + probs[:,:,1])
+        ratProbs['normalized_probs'] = normalized_probs
 
-		pro_rules = rat_data['valX'][0,:,0]
-		postRNNdata['pro_rules'] = pro_rules
+        hit_rate = np.zeros(probs.shape[1])
+        right = rat_data['valTrueY'][0,:] == 1
+        left = rat_data['valTrueY'][0,:] == 0
+        hit_rate[left] = normalized_probs[0,left,0]
+        hit_rate[right] = normalized_probs[0,right,1]
+        ratProbs['hit_rate'] = hit_rate
 
-		pro_prob = np.copy(hit_rate)
-		anti_prob = np.copy(hit_rate)
-		pro_rules = pro_rules.astype(bool)
-		pro_prob[np.logical_not(pro_rules)] = None
-		anti_prob[pro_rules] = None
-		postRNNdata['pro_prob'] = pro_prob
-		postRNNdata['anti_prob'] = anti_prob
+        T = rat_data['valX'].shape[1]
 
-	return postRNNdata
+        switches = []
+        p2a_switch = []
+        a2p_switch = []
+        pro_rule = rat_data['valX'][0,0,0]
+        for i in xrange(T):
+            if rat_data['valX'][0,i,0] != pro_rule:
+                switches.append(i)
+                pro_rule = rat_data['valX'][0,i,0]
+                if i > trial_window and i < T - trial_window:
+                    if pro_rule == 0:
+                        p2a_switch.append(i)
+                    else:
+                        a2p_switch.append(i)
+        ratProbs['switches'] = switches
+        ratProbs['p2a_switch'] = p2a_switch
+        ratProbs['a2p_switch'] = a2p_switch
+
+        pro_rules = rat_data['valX'][0,:,0]
+        ratProbs['pro_rules'] = pro_rules
+
+        pro_prob = np.copy(hit_rate)
+        anti_prob = np.copy(hit_rate)
+        pro_rules = pro_rules.astype(bool)
+        pro_prob[np.logical_not(pro_rules)] = None
+        anti_prob[pro_rules] = None
+        ratProbs['pro_prob'] = pro_prob
+        ratProbs['anti_prob'] = anti_prob
+
+        p2a_prob = calculatePerformance(p2a_switch, hit_rate, trial_window)
+        a2p_prob = calculatePerformance(a2p_switch, hit_rate, trial_window)
+
+        ratProbs['p2a_prob'] = p2a_prob
+        ratProbs['a2p_prob'] = a2p_prob
+
+    return postRNNdata
+
+def calculatePerformance(switch_index, hit_rate, trial_window):
+    switch_prob = np.zeros(trial_window*2+1) 
+    # index i --> trial from switch = -trial_window + i
+    switch_prob = np.zeros((2*trial_window+1))
+
+    for i in switch_index:
+        switch_prob += hit_rate[i-trial_window:i+trial_window+1]
+    switch_prob /= len(switch_index)
+    return switch_prob
+
+def meanPerformance(postRNNdata):
+    size = postRNNdata[postRNNdata.keys()[0]]['p2a_prob'].shape[0]
+    p2a_mean = np.zeros(size)
+    a2p_mean = np.zeros(size)
+    for ratname in postRNNdata.keys():
+        p2a_mean += postRNNdata[ratname]['p2a_prob']
+        a2p_mean += postRNNdata[ratname]['a2p_prob']
+    p2a_mean /= len(postRNNdata)
+    a2p_mean /= len(postRNNdata)
+    return p2a_mean, a2p_mean
 
 def affine_forward(x, w, b):
     """
